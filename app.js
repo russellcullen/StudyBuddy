192b2c3757893726b68d4c15963d95c727e4364b
Russell Cullen <russellscullen@gmail.com>
2013-01-23 15:55:41 -0500

    Add facebook login with popup

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , everyauth = require('everyauth');
+
+users = {}
+
+everyauth.everymodule
+  .findUserById( function (id, callback) {
+    callback(null, users[id]);
+  });
+
+everyauth.facebook
+  .appId("121513184688795")
+  .appSecret("3ddb937b0b0bd28c7900a17b66930819")
+  .popup(true)
+  .findOrCreateUser( function (session, accessToken, accessTokExtra, fbUserMetadata) {
+    console.log(fbUserMetadata);
+    users[fbUserMetadata.id] = fbUserMetadata;
+    return users[fbUserMetadata.id];
+  })
+  .redirectPath('/login');
 
 var app = express();
 
@@ -19,6 +38,9 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser()); 
+  app.use(express.session({ secret: 'russell'}));
+  app.use(everyauth.middleware(app));
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,6 +50,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/login', routes.login);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
