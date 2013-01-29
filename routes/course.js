acb47b35b5ad175124e5003fd4904ef885713314
Russell Cullen <russellscullen@gmail.com>
2013-01-28 23:39:17 -0500

    Status implemented, cleaned up documentation for routes

--- routes/course.js
+++ routes/course.js
@@ -1,18 +1,22 @@
 /*
  *  Routes related to courses
  */
+var async = require('async');
 var models = require('../lib/models');
 var Course = models.Course;
+var Status = models.Status;
 
 /*
- * GET for creating course
- */ 
+ * Create courses page.
+ *    Type : GET
+ */
 exports.create = function(req, res){
   res.render('create-course', { title: 'Create Course'});
 };
 
 /*
- * POST for creating course
+ * Create courses route.
+ *    Type : POST
  */
 exports.save = function(req, res){
   var course = new Course({
@@ -21,31 +25,54 @@ exports.save = function(req, res){
     public: req.body.privacy,
     students: [req.user._id]
   });
-  course.save(function(err) {
+  course.save(function (err) {
     if (err) return next(err); // Bubble error up
-    res.redirect('/my-courses');
-  });
-}
-
-/*
- * POST for joining course
- */
-exports.join = function(req, res){
-  Course.findOne({ '_id' : req.body.courseId}, function (err, course) {
-    course.addStudent(req.user._id, function (err) {
-      if (err) return next(err);
+    var status = new Status({
+      course: course._id,
+      user: req.user._id,
+      status: req.body.status
+    });
+    status.save(function (err) {
       res.redirect('/my-courses');
     });
   });
 }
 
 /*
- * GET show all courses
+ * Join courses route.
+ *    Type : POST
+ */
+exports.join = function(req, res){
+  Course.findOne({ '_id' : req.body.courseId}, function (err, course) {
+    course.addStudent(req.user._id, function (err) {
+      if (err) return next(err);
+      var status = new Status({
+        course: course._id,
+        user: req.user._id,
+      });
+      status.save(function (err) {
+        res.redirect('/my-courses');
+      });
+    });
+  });
+}
+
+/*
+ * Course listing page.
+ *    Type : GET
  */
 exports.public = function(req, res){
   var page = req.query.page || 0;
   Course.find({public : true}, null, {limit : 20, skip : page*20}, function (err, courses){
     if (err) return next(err);
-    res.render('courses', { title: 'Courses', courses: courses});
+    async.map(courses, function (course, cb) {
+      if (!req.loggedIn) return cb(err, course);
+      Status.findOne({'course' : course._id, 'user' : req.user._id}, function (err, status) {
+        if (status) course.status = status.status;
+        cb(err, course);
+      });
+    }, function (err, courses) {
+      res.render('courses', { title: 'Courses', courses: courses});
+    });
   });
 }
--- routes/user.js
+++ routes/user.js
@@ -1,16 +1,33 @@
 /*
  * User related routes.
  */
+var async = require('async');
 var models = require('../lib/models');
 var Course = models.Course;
+var Status = models.Status;
 
+/*
+ * User home page.
+ *    Type : GET
+ */
 exports.home = function(req, res){
   res.render('home', { title: 'Home'});
 };
 
+/*
+ * User courses page.
+ *    Type : GET
+ */
 exports.courses = function(req, res){
   Course.find({ students : req.user._id }, function (err, courses) {
     if (err) return next(err);
-    res.render('courses', { title : 'My Courses', courses : courses });
+    async.map(courses, function (course, cb) {
+      Status.findOne({'course' : course._id, 'user' : req.user._id}, function (err, status) {
+        course.status = status.status;
+        cb(err, course);
+      });
+    }, function (err, courses) {
+      res.render('courses', { title : 'My Courses', courses : courses });
+    });
   });
 };
