d5f53f76871e37bb038f578f23b45a88a9eee360
Russell Cullen <russellscullen@gmail.com>
2013-02-06 21:26:08 -0500

    Remove post on feed. More styling changes

--- public/javascripts/ajax.js
+++ public/javascripts/ajax.js
@@ -2,12 +2,14 @@
  * AJAX file for needed ajax requests
  */
 (function(window) {
-  window.pollFeed = function () {
+  window.pollFeed = function (userId) {
     $.ajax({ 
       url: "/api/feed", 
       success: function(data, status, res){
-        updateFeed(data);
-        setTimeout('pollFeed()', 5000);
+        updateFeed(data, userId);
+        setTimeout(function() {
+          pollFeed(userId);
+        }, 5000);
       }, 
       dataType: "json",
       timeout: 30000 
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -3,28 +3,37 @@
  */
 (function(window) {
   // Creates a post div from post JSON
-  window.createFeedPost = function (post) {
-    var $post = $('<div style="display: none;" class="post" id="' + post._id + '"></div>');
-    $close = $("<a href='#removeModal' role='button' data-toggle='modal' data-id='" + post._id + "' class='close remove-post'> &times;</a>")
-    $close.on('click', function() {
-      var id = $(this).data('id');
-      $('#post-input').val(id);
-    });
-    $post.append($close);
-    $post.append("<h4><a href='/course/"+post.course._id+"'>"+post.course.name+"</a></h4>");
+  window.createFeedPost = function (post, userId) {
+    var $outer = $('<div style="display: none;" class="post" id="' + post._id + '"></div>');
+    var $post = $('<div class="row"></div>');
+    $outer.append($post);
+    if (String(post.from._id) == String(userId)) {
+      $close = $("<a href='#removeModal' role='button' data-toggle='modal' data-id='" + 
+        post._id + "' class='close remove-post'> &times;</a>")
+      $close.on('click', function() {
+        var id = $(this).data('id');
+        $('#post-input').val(id);
+      });
+      $post.append($close);
+    }
+    $post.append("<a href='/user/"+post.from._id+"'><img src='"+ 
+      post.from.gravatar + "&s=40' class='pull-left profile-left'></img></a></h4>");
+    $post.append("<strong><a href='/user/"+post.from._id+"'>"+post.from.name+"</a></strong>");
+    $post.append(" posted to ");
+    $post.append("<strong><a href='/course/"+post.course._id+"'>"+post.course.name+"</a></strong>");
     $post.append("<p>"+post.message+"</p>");
-    $post.append("<h6>"+post.from.name+"</h6>");
-    return $post;
+
+    return $outer;
   }
 
   // Adds new posts from JSON Array of posts to div with id of feed
-  window.updateFeed = function (posts) {
+  window.updateFeed = function (posts, userId) {
     tmp = []
     posts.some(function (post) {
-      var $post = createFeedPost(post);
       if ($('#' + post._id).length > 0) {
         return true;
       } else {
+        var $post = createFeedPost(post, userId);
         tmp.unshift($post)
       }
     });
